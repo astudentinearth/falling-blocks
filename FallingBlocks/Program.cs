e99e51f85eb95abf5366c185fa29e31ca115374c
astudentinearth <burak.yn.dev@gmail.com>
2024-02-18 17:04:02 +0300

    Initial commit

--- FallingBlocks/Program.cs
+++ FallingBlocks/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Timers;
+internal class Program
+{
+    private static int count = 0;
+    private static System.Timers.Timer loop = new System.Timers.Timer();
+    private static void Main(string[] args)
+    {
+        Renderloop();
+        Console.ReadKey();
+    }
+
+    private static void Renderloop()
+    {
+        loop.Elapsed += (sender, e) => { count++; Redraw(); Console.WriteLine("Hello world"); };
+        loop.Interval = 250;
+        loop.Start();
+    }
+    public static void Redraw()
+    {
+        Console.Clear();
+        Console.WriteLine(count.ToString());
+    }
+}
